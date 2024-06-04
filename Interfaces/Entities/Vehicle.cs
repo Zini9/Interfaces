df6d6b4c048350bcacf9bc8060f4b049aa5f0bc6
Guilherme Zini <guilhermerosazini@gmail.com>
2024-06-04 20:50:02 -0300

    Projeto criado

---
+++ Interfaces/Entities/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Entities
+{
+    internal class Vehicle
+    {
+        public string Model { get; set; }
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
