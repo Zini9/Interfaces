0e3f6e1631911f9d3647cec14329d588f56ecf6d
Guilherme Zini <guilhermerosazini@gmail.com>
2024-06-04 21:09:29 -0300

    Classes BrazilTaxServices - CarRental - RentalService Adicionadas

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+using Interfaces.Entities;
 
 namespace Interfaces
 {
@@ -10,6 +12,15 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter rental data");
+            Console.WriteLine("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pick-up (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(),"dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
         }
     }
 }
---
+++ Interfaces/Services/RentalService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Services
+{
+    internal class RentalService
+    {
+
+        public double PricePerHour { get; private set; } // (private) Proteger para que valores não sejam alterados
+        public double PricePerDay { get; private set; }
+
+    }
+}
